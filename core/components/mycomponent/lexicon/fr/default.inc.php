2912f23812c50f36003f2cadde8ea4c7a90e51da
bobray <bobray@softville.com>
2011-01-22 02:02:04 -0500

    Updated language files

--- core/components/mycomponent/lexicon/fr/default.inc.php
+++ core/components/mycomponent/lexicon/fr/default.inc.php
@@ -24,20 +24,20 @@
  * @package mycomponent
  * @subpackge lexicon
  */
-$_lang['np_no_error_tpl'] = 'Impossible de trouver &amp;errortpl: [[+tpl]]';
-$_lang['np_main_header'] = 'Créer/éditer une ressource';
-$_lang['np_error_presubmit'] = 'Désolé… il y a eu un (ou plusieurs) problème(s) à la création du formulaire:';
-$_lang['np_error_submit'] = 'Désolé… il y a eu un (ou plusieurs) problème(s) avec votre "submission":';
-$_lang['np_error_required'] = 'Le champ [[+name]] est requis.';
-$_lang['np_no_resource_group'] = 'Impossible de trouver le groupe de ressources [[+name]].';
-$_lang['np_no_resource'] = 'Erreur lors de l\'obtention de la ressource: [[+id]].';
-$_lang['np_no_template_name'] = 'Erreur lors de l\'obtention du modèle: [[+name]].';
-$_lang['np_no_tvs'] = 'Vous vouliez ordonner les variables de modèle, mais ce modèle n\'en contient aucune.';
-$_lang['np_unauthorized'] = 'Vous n\'êtes pas autorisé à publier des articles.';
-$_lang['np_folder_not_sent'] = 'Vous avez configuré &amp;template "à parent" mais n\'avez pas inclue le "paramètre de dossier".';
-$_lang['np_resource_save_failed'] = 'Une erreur est survenue lors de la sauvegarde de la ressource.';
-$_lang['np_to_template_id'] = 'Il n\'y a pas de modèle ayant pour ID: [[+id]].';
-$_lang['np_to_template_name'] = 'Il n\'y a pas de modèle ayant pour nom: [[+name]].';
+$_lang['mc_no_error_tpl'] = 'Impossible de trouver &amp;errortpl: [[+tpl]]';
+$_lang['mc_main_header'] = 'Créer/éditer une ressource';
+$_lang['mc_error_presubmit'] = 'Désolé… il y a eu un (ou plusieurs) problème(s) à la création du formulaire:';
+$_lang['mc_error_submit'] = 'Désolé… il y a eu un (ou plusieurs) problème(s) avec votre "submission":';
+$_lang['mc_error_required'] = 'Le champ [[+name]] est requis.';
+$_lang['mc_no_resource_group'] = 'Impossible de trouver le groupe de ressources [[+name]].';
+$_lang['mc_no_resource'] = 'Erreur lors de l\'obtention de la ressource: [[+id]].';
+$_lang['mc_no_template_name'] = 'Erreur lors de l\'obtention du modèle: [[+name]].';
+$_lang['mc_no_tvs'] = 'Vous vouliez ordonner les variables de modèle, mais ce modèle n\'en contient aucune.';
+$_lang['mc_unauthorized'] = 'Vous n\'êtes pas autorisé à publier des articles.';
+$_lang['mc_folder_not_sent'] = 'Vous avez configuré &amp;template "à parent" mais n\'avez pas inclue le "paramètre de dossier".';
+$_lang['mc_resource_save_failed'] = 'Une erreur est survenue lors de la sauvegarde de la ressource.';
+$_lang['mc_to_template_id'] = 'Il n\'y a pas de modèle ayant pour ID: [[+id]].';
+$_lang['mc_to_template_name'] = 'Il n\'y a pas de modèle ayant pour nom: [[+name]].';
 
 
 
