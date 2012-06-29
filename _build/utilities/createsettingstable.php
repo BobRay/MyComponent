ac3e6c5b59db99d1c16d88aba48b0ee1898b1b58
bobray <bobray@softville.com>
2012-06-29 01:56:24 -0500

    added createsettingstable utility

---
+++ _build/utilities/createsettingstable.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * CreateSettingsTable
+ * Copyright 2012 Bob Ray
+ *
+ * CreateSettingsTable is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * CreateSettingsTable is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * CreateSettingsTable; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package mycomponent
+ * @author Bob Ray <http://bobsguides.com>
+
+ *
+ * Description: The CreateSettingsTable snippet creates a table of
+ * System Settings and descriptions to paste into tutorials and documentation.
+ * The table is based on the settings in a transport.settings.php file
+ * and on a language file to pull descriptions from.
+ * /
+
+/*
+  Modified: June, 2012
+  $packageName = 'mycomponent';
+*/
+
+$base = 'c:/xampp/htdocs/addons/assets/mycomponents/subscribe/';
+$settingsFile = '_build/data/transport.settings.php';
+$settingsFile = $base . $settingsFile;
+$languageFile = 'core/components/subscribe/lexicon/en/default.inc.php';
+$languageFile = $base . $languageFile;
+$rewriteCodeFile = false;
+$codeFile = $settingsFile;
+
+
+$settingsInjected = false; /* This will be set automatically if settings are injected */
+
+$outsideModx = false;
+/* @var $modx modX */
+if (!defined('MODX_CORE_PATH')) {
+    $outsideModx = true;
+    define ('MODX_BASE_URL', '');
+    define ('MODX_MANAGER_URL', '');
+    define ('MODX_ASSETS_URL', '');
+    define ('MODX_CONNECTORS_URL', '');
+    require_once dirname(dirname(__FILE__)).'/build.config.php';
+    require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+    $modx= new modX();
+    $modx->initialize('mgr');
+    $modx->setLogLevel(modX::LOG_LEVEL_INFO);
+    $modx->setLogTarget('ECHO');
+    echo '<pre>'; /* used for nice formatting for log messages  */
+}
+    if (! defined('DS')) {
+        define('DS', DIRECTORY_SEPARATOR);
+    }
+
+if (!file_exists($settingsFile)) {
+    echo 'Could not find settings file';
+}
+if (!file_exists($languageFile)) {
+    echo 'Could not get language file';
+}
+$settings = include $settingsFile;
+if (empty($settings)) {
+    return 'No settings';
+}
+
+include $languageFile;
+if (empty($_lang)) {
+    return 'No language strings';
+}
+
+$tableTpl = "\n\n<table class=\"properties\">
+    <tr><th>Setting</th><th>Description</th><th>Default</th></tr>
+[[+rows]]
+</table>";
+
+$rowTpl = '
+    <tr>
+        <td>[[+name]]</td>
+        <td>[[+description]]</td>
+        <td>[[+default]]</td>
+    </tr>';
+
+
+$setting = array();
+$findFields = array(
+    '[[+name]]',
+    '[[+description]]',
+    '[[+default]]',
+);
+$rows = '';
+
+/* wrap long lines in comment block */
+function wrapComment($text, $width = 70) {
+    $textArray = explode("\n",$text);
+    foreach ($textArray as $k => $v) {
+        $textArray[$k] = wordwrap ( $v , $width,"\n *    ");
+    }
+    return implode("\n", $textArray);
+
+
+}
+function parseDesc($text, &$fields) {
+    // echo "\nTEXT: " . $text . "\n";
+    $fields = array();
+    $matches = array();
+    if (isset($_lang[$text])) {
+       $text = $_lang[$text];
+    }
+
+    if (strstr($text,'~~') ) {
+
+        preg_match("/~~(.+)$/",$text,$matches);
+        $text = $matches[1];
+        //echo "\nTEXT: " . $text . "\n";
+    }
+    /* ~~ and prior text is now removed */
+    /* get default and remove it from description */
+    if (stristr($text,'default')) {
+        // echo "\nTEXT: " . $text . "\n";
+        $pattern = '/(.+)[Dd]efault[:\s](.+)$/';
+        preg_match($pattern, $text, $matches);
+        $fields['desc'] = $matches[1];
+
+        $fields['default'] = $matches[2];
+    } else {
+        $fields['desc'] = $text;
+    }
+    $fields['desc'] = trim($fields['desc'],"\.\'\"\;\: ");
+
+    if (isset($fields['default'])) {
+        $fields['default'] = rtrim($fields['default'],"\.\'\"\;\: ");
+        $fields['default'] = ltrim($fields['default'],"\'\"\;\: ");
+    }
+    //echo "DESCRIPTION: " . $fields['desc'] . "\n";
+    // echo "DEFAULT: " . $fields['default'] . "\n";
+}
+
+//echo "COUNT: " . count($settings) . "\n";
+$settingsComment = '';
+foreach($settings as $settingObject) {
+    $setting = $settingObject->toArray();
+    $fields = array();
+
+    $replaceFields = array(
+        'name' => $setting['key'],
+        'description' => '',
+        'default' => $setting['value'],
+    );
+
+    $replaceFields['default'] = $replaceFields['default'] == '999'? '(set automatically)' : $replaceFields['default'];
+    $languageKey = 'setting_' . $replaceFields['name'] . '_desc';
+    if (isset($_lang[$languageKey])) {
+        $replaceFields['description'] = $_lang[$languageKey];
+    }
+    parseDesc($replaceFields['description'], $fields);
+    if (isset($fields['desc']) && !empty($fields['desc'])) {
+        $replaceFields['description'] = $fields['desc'];
+    }
+    if (isset($fields['default']) && empty($replaceFields['default'])) {
+            $replaceFields['default'] = $fields['default'];
+    }
+    $row = str_replace($findFields,$replaceFields,$rowTpl);
+
+    $rows .= $row;
+
+    /* add to properties comment */
+
+    $settingsComment .= ' * @property &' . $setting['key'] . ' ' . $setting['xtype'];
+    $settingsComment .= ' -- ' . $fields['desc'] . '; Default: ';
+    $settingsComment .= empty($fields['default'])? '(empty).' : $fields['default'];
+    $settingsComment .= ".\n";
+
+
+}
+
+$output = str_replace('[[+rows]]', $rows, $tableTpl);
+if ($rewriteCodeFile && !empty($codeFile) ) {
+    $content = file_get_contents($codeFile);
+    $count = 0;
+    if (!empty($content) && !empty($settingsComment)) {
+        $settingsComment = wrapComment($settingsComment);
+        $content = str_replace('[[+properties]]', "Properties:\n" . $settingsComment, $content, $count);
+
+        if ($count == 1) {
+
+            $fp = fopen($codeFile, 'w');
+            fwrite($fp, $content);
+            fclose($fp);
+            $settingsInjected = true;//echo $content;
+        } else {
+            $settingsInjected = false;
+        }
+    }
+}
+$output .= "\n\n/* Properties\n\n";
+$output .= $settingsComment;
+$output .= "\n */\n";
+if ($settingsInjected) {
+    $output .=  "\n\n Properties injected into code file\n\nScroll up for table and properties comment";
+} else {
+    $output .=  "\n\n" . "No properties tag or multiple properties tags\nFile unchanged\nScroll up for table and properties comment\n\n";
+
+}
+echo $output;
+//echo print_r($_lang);
+
+return '';
+
+
