cd25f44380faad0696e9c70f4814f92c53a024c9
bobray <bobray@softville.com>
2012-07-31 17:25:56 -0500

    updated sample config file

--- _build/utilities/config/componentname.config.php
+++ _build/utilities/config/componentname.config.php
@@ -1,7 +1,7 @@
 <?php
 /* use this as an example. Copy to componentname.config.php in this directory */
 
-$components = array (
+$components = array(
     /* These are used to define the package and set values for placeholders */
     'packageName' => 'CacheMaster',
     'packageNameLower' => 'cachemaster',
@@ -10,10 +10,12 @@ $components = array (
     'copyright' => '2012',
     'createdon' => strftime('%m-%d-%Y'),
     'offerAbort' => false, /* Show package name and ask user to confirm before running */
+    /* Change this if you need to alter any tpl files. User new dir. under _build/utilities */
+    'tplDir' => 'buildtpls',
 
-/* Set directory permissions for project directories */
-    'dirPermission' => 0755,  /* No quotes!! */
-    'filePermission' => 0644,  /* No quotes!! */
+    /* Set directory permissions for project directories */
+    'dirPermission' => 0755, /* No quotes!! */
+    'filePermission' => 0644, /* No quotes!! */
 
     /* Define source directories (mycomponent root and core directories) */
     'source' => MODX_ASSETS_PATH . 'mycomponents/mycomponent/', /* path to MyComponent source files */
@@ -23,27 +25,40 @@ $components = array (
     'initialize' => true,
     'defaultStuff' => array(
         '_build' => true, /* build directory for transport package */
-        'lexicon' => true, /* location for lexicon files */
+        'utilities' => false, /* copy utilities to target dir -- Usually unnecessary */
+        'lexicon' => true, /* create lexicon directory */
         'docs' => true, /* readme.txt, license, changelog, and/or tutorial(s) */
         'readme.md' => true, /* Description file for GitHub project home page */
         'languages' => 'en,fr', /* only list languages for which you have language files */
     ),
-    /* Define optional directories -- comment out ones you don't need */
-    'optionalDirs' => array(
-        'css',
-        'js',
-        'images',
-        'audio',
-        'video',
-        /* extra script resolver(s) to be run during install */
-        'resolvers',
-        /* validators can abort the install after checking conditions */
-        'validators',
-        /* needed if you will interact with user during the install */
-        'install.options',
+    /* Define optional directories to create under assets
+     * add your own as needed
+     * set to true to create directory
+     * set to false to skip
+     * Empty js and css files will be created
+     */
+    'assetsDirs' => array(
+        'css' => true,
+        'js' => true,
+        'images' => true,
+        'audio' => true,
+        'video' => true,
+        'themes' => true,
     ),
+    /* extra script resolver(s) to be run during install
+    * comma-separated list of validator names or '' (no prefix of suffix) */
+    'resolvers' => '',
+
+    /* validators can abort the install after checking conditions
+     * comma-separated list of validator names or '' (no prefix of suffix) */
+    'validators' => '',
+
+    /* install.options is needed if you will interact with user during the install */
+    /* set to 'install.options' or '' */
+    'install.options' => 'install.options',
+
     /* suffixes for files */
-    'suffixes' => array (
+    'suffixes' => array(
         'plugin' => '.php',
         'snippet' => '.php',
         'chunk' => '.html',
@@ -54,10 +69,13 @@ $components = array (
     ),
 
     /* These control the creation of elements */
-    'createFiles' => true,  /* create element files */
-    'createObjects' => true, /* also create objects in MODX */
-    'makeStatic' => 'CacheMaster', /* Comma-separated list of objects to set as static */
-    'allStatic' => true, /* will make all objects static - makeStatic will be ignored */
+    'createElementFiles' => true, /* create element files */
+    'createElementObjects' => true, /* also create objects in MODX */
+    'makeStatic' => 'CacheMaster', /* Comma-separated list of elements to set as static */
+    'createResourceObjects' => false,
+    'createResourceFiles' => false,
+    'allStatic' => true, /* will make all element objects static - makeStatic will be ignored */
+
 
     /* comma-separated lists of the actual Element Names */
     'elements' => array(
@@ -66,7 +84,7 @@ $components = array (
         'tvs' => '',
         'templates' => '',
         'chunks' => '',
-        ),
+    ),
     /* comma-separated lists of the actual Resource Names */
     'resources' => array(
 
@@ -75,7 +93,7 @@ $components = array (
      * to create */
     'otherObjects' => array(
         'menus' => '',
-        'systemEvents' => '',
+        'systemEvents' => '', /* only *new* System Events to be created */
         'propertySets' => '',
         'systemSettings' => '',
 
