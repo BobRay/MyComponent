ee8918ca13b994e897d6cbf0db4e3605c70631c3
bobray <bobray@softville.com>
2012-08-31 00:30:34 -0500

    set rewriteLexiconFiles to true

--- _build/utilities/config/example.config.php
+++ _build/utilities/config/example.config.php
@@ -262,7 +262,7 @@ $components = array(
     /* ******************************************* */
     /* These settings are used by LexiconHelper */
     'rewriteCodeFiles' => false, // remove ~~descriptions
-    'rewriteLexiconFiles' => false, // automatically add missing strings to lexicon files
+    'rewriteLexiconFiles' => true, // automatically add missing strings to lexicon files
     /* ******************************************* */
 
     /* If your extra needs Menus, set this to true, create them
