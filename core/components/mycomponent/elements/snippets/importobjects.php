9e3127ce698bd8180d7cfb0dc9a57dff9c96f696
bobray <bobray@softville.com>
2012-10-25 05:11:36 -0500

    implemented importObjects

---
+++ core/components/mycomponent/elements/snippets/importobjects.php
@@ -0,0 +1,89 @@
+<?php
+
+/* set start time */
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tstart = $mtime;
+set_time_limit(0);
+$mem_usage = memory_get_usage();
+
+/* @var $modx modX */
+
+if (!defined('MODX_CORE_PATH')) {
+    $path1 = dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/_build/build.config.php';
+    if (file_exists($path1)) {
+        include $path1;
+    } else {
+        $path2 = dirname(dirname(dirname(dirname(__FILE__)))) . '/_build/build.config.php';
+        if (file_exists($path2)) {
+            include($path2);
+        }
+    }
+    if (!defined('MODX_CORE_PATH')) {
+        die('[importobjects.php] Could not find build.config.php');
+    }
+    require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+    $modx = new modX();
+    /* Initialize and set up logging */
+    $modx->initialize('mgr');
+    $modx->setLogLevel(xPDO::LOG_LEVEL_INFO);
+    $modx->setLogTarget(XPDO_CLI_MODE
+        ? 'ECHO'
+        : 'HTML');
+
+    /* This section will only run when operating outside of MODX */
+    if (php_sapi_name() == 'cli') {
+        /* Set $modx->user and $modx->resource to avoid
+         * other people's plugins from crashing us */
+        $modx->getRequest();
+        $homeId = $modx->getOption('site_start');
+        $homeResource = $modx->getObject('modResource', $homeId);
+        if ($homeResource instanceof modResource) {
+            $modx->resource = $homeResource;
+        } else {
+            echo "\nNo Resource\n";
+        }
+    } else {
+        echo "\n<pre>\n";
+    }
+
+}
+
+require_once $modx->getOption('mc.core_path', null, $modx->getOption('core_path') . 'components/mycomponent/') . 'model/mycomponent/mycomponentproject.class.php';
+
+$project = new MyComponentProject($modx);
+$project->init();
+
+//$project->removeObjects();
+
+
+$dryRun = false; /* true is the default -- set to false for actual import */
+/* Comma-separated list of elements to process (snippets,plugins,chunks,templates) */
+$toProcess = 'snippets';
+/* path to elements directory to import -- if empty, project's elements dir will be used */
+$directory = $modx->getOption('mc.core', null,
+    $modx->getOption('core_path') . 'components/mycomponent/') . 'elements/';
+
+if (!empty($directory) && ! is_dir($directory)) {
+    die ('[importobjects.php] ' . "\$directory is not a directory");
+}
+
+$project->importObjects($toProcess, $directory, $dryRun);
+
+// echo print_r(ObjectAdapter::$myObjects, true);
+
+echo "\n\nInitial Memory Used: " . round($mem_usage / 1048576, 2) . " megabytes";
+$mem_usage = memory_get_usage();
+$peak_usage = memory_get_peak_usage(true);
+echo "\nFinal Memory Used: " . round($mem_usage / 1048576, 2) . " megabytes";
+echo "\nPeak Memory Used: " . round($peak_usage / 1048576, 2) . " megabytes";
+/* report how long it took */
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tend = $mtime;
+$totalTime = ($tend - $tstart);
+$totalTime = sprintf("%2.4f s", $totalTime);
+echo "\nTotal time: " . $totalTime;
+
