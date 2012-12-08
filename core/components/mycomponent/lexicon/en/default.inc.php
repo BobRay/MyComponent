74ae70a46553c71d74a56940fef565866c9e28ce
bobray <bobray@softville.com>
2012-12-07 20:22:56 -0600

    added lexicon file

---
+++ core/components/mycomponent/lexicon/en/default.inc.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * en default topic lexicon file for MyComponent extra
+ *
+ * Copyright 2012 by Bob Ray <http://bobsguides.com>
+ * Created on 12-05-2012
+ *
+ * MyComponent is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MyComponent is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MyComponent; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package mycomponent
+ */
+
+/**
+ * Description
+ * -----------
+ * en default topic lexicon strings
+ *
+ * Variables
+ * ---------
+ * @var $modx modX
+ * @var $scriptProperties array
+ *
+ * @package mycomponent
+ **/
+
+
+/* used in MyComponent or its included classes  */
+$_lang['mc_you_must_specify_a_project_name'] = 'You must specify a project name';
+$_lang['mc_project_already_exists'] = 'Project already exists';
+
+/* used in Bootstrap or its included classes  */
+$_lang['mc_Updated'] = 'Updated';
+$_lang['mc_Creating'] = 'Creating';
+$_lang['mc_begin_file_content'] = '******** Begin File Content ********';
+$_lang['mc_end_file_content'] = '******** End File Content ********';
+$_lang['mc_could_not_write_file'] = 'Could not write file';
+$_lang['mc_creating'] = 'Creating';
+$_lang['mc_objects'] = 'objects';
+$_lang['mc_property_set_nf'] = 'Could not find Property Set';
+$_lang['mc_asked_for_unknown_intersect_type'] = 'Asked for unknown intersect type';
+$_lang['mc_error_creating_intersect'] = 'Error creating intersect';
+$_lang['mc_could_not_get_main_object'] = 'Could not get main object';
+$_lang['mc_could_not_get_subsidiary_object'] = 'could not get subsidiary object';
+$_lang['mc_intersect_already_exists_for'] = 'Intersect already exists for';
+$_lang['mc_created_intersect'] = 'Created Intersect';
+$_lang['mc_could_not_create_intersect_for'] = 'Could not create intersect for';
+$_lang['mc_project'] = 'Project';
+$_lang['mc_updated_projects_file'] = 'Updated projects.php file';
+$_lang['mc_created_projects_file'] = 'Created projects.php file';
+$_lang['mc_mycomponent_not_installed_create_new'] = 'MyComponent must be installed to create a new MyComponent Project!';
+$_lang['mc_action'] = 'Action';
+$_lang['mc_bootstrap'] = 'Bootstrap';
+$_lang['mc_removing_objects'] = 'Removing Project Objects from MODX';
+$_lang['mc_processing_contexts'] = 'Processing Contexts';
+$_lang['mc_processing_categories'] = 'Processing Categories';
+$_lang['mc_creating_category_resolver'] = 'Creating Category resolver';
+$_lang['mc_categoryresolver_tpl_empty'] = 'categoryresolver tpl is empty';
+$_lang['mc_already_exists'] = 'already exists';
+$_lang['mc_processing'] = 'Processing';
+$_lang['mc_in_category'] = 'in category';
+$_lang['mc_no'] = 'No';
+$_lang['mc_found_in_category'] = 'found in category';
+$_lang['mc_self_nf'] = 'Cannot find myself';
+$_lang['mc_category_nf'] = 'Could not find category';
+$_lang['mc_no_elements_to_process'] = 'No Elements to process';
+$_lang['mc_processing_transport_files_for_category'] = 'Processing Transport Files for Category';
+$_lang['mc_processing_object'] = 'Processing object';
+$_lang['mc_set_static_path_to'] = 'Set static path to';
+$_lang['mc_file_already_exists'] = 'File already exists';
+$_lang['mc_would_be_creating'] = 'Would be creating';
+$_lang['mc_skipping_static_element_code_file'] = 'Skipping code file for static element';
+$_lang['mc_removed'] = 'Removed';
+$_lang['mc_processing_new_system_settings'] = 'Processing New System Settings';
+$_lang['mc_processing_system_settings'] = 'Processing System Settings';
+$_lang['mc_system_setting_nf'] = 'Could not find System Setting with key';
+$_lang['mc_processing_context_settings'] = 'Processing Context Settings';
+$_lang['mc_processing_new_system_events'] = 'Processing New System Events';
+$_lang['mc_created_se'] = 'Created System Event';
+$_lang['mc_could_not_save_se'] = 'Could not save System Event';
+$_lang['mc_could_not_create_se'] = 'Could not create System Event';
+$_lang['mc_updated_se'] = 'Updated System Event';
+$_lang['mc_failed_to_update_se'] = 'Failed to updated System Event';
+$_lang['mc_se_already_exists'] = 'System Event already exists';
+$_lang['mc_processing_system_events'] = 'Processing System Events';
+$_lang['mc_processing_elements'] = 'Processing Elements';
+$_lang['mc_processing_resources'] = 'Processing Resources';
+$_lang['mc_outdated_category'] = 'Outdated Category';
+$_lang['mc_updated_category_for'] = 'Updated category for';
+$_lang['mc_failed_to_create_object'] = 'Failed to create object';
+$_lang['mc_created'] = 'Created';
+$_lang['mc_set_tv_values'] = 'Set TvValues for resource';
+$_lang['mc_no_id_for'] = 'No ID for';
+$_lang['mc_class_file_nf'] = 'Could not find';
+$_lang['mc_tpl_nf'] = 'Could not find Tpl for';
+$_lang['mc_parent_nf'] = 'Could not find parent for resource';
+$_lang['mc_creating_resource_resolver'] = 'Creating resource resolver';
+$_lang['mc_resourceresolvertpl_empty'] = 'resourceresolver tpl is empty';
+$_lang['mc_could_not_get_resource_with_method'] = 'Could not get resource with';
+$_lang['mc_no_resources_found'] = 'No Resources found';
+$_lang['mc_processing_menus'] = 'Processing Menus';
+$_lang['mc_menu_has_no_action'] = 'Menu has no action';
+$_lang['mc_menu_action_no_namespace'] = 'Menu Action has no namespace';
+$_lang['mc_created_menu'] = 'Created Menu';
+$_lang['mc_menu_already_exists'] = 'Menu already exists';
+$_lang['mc_removed_menu_item'] = 'Removed menu item';
+$_lang['mc_removed_action'] = 'Removed action for menu';
+$_lang['mc_creating_resolver'] = 'Creating resolver';
+$_lang['mc_empty_genericresolver'] = 'genericresolver tpl is empty';
+$_lang['mc_resolver'] = 'resolver';
+$_lang['mc_creating_validator'] = 'Creating validator';
+$_lang['mc_empty_genericvalidator'] = 'genericvalidator tpl is empty';
+$_lang['mc_validator'] = 'validator';
+$_lang['mc_creating_build_files'] = 'Creating build files';
+$_lang['mc_already already exists'] = 'already exists';
+$_lang['mc_creating_lexicon_files'] = 'Creating Lexicon files';
+$_lang['mc_file'] = 'file';
+$_lang['mc_creating_doc_files'] = 'Creating doc files';
+$_lang['mc_jsmin_nf'] = 'Could not find jsmin.class.php';
+$_lang['mc_file_nf'] = 'Could not find file';
+$_lang['mc_creating_assets_directories'] = 'Creating Assets directories';
+$_lang['mc_directory'] = 'directory';
+$_lang['mc_creating_install_options'] = 'Creating Install Options';
+$_lang['mc_creating_validators'] = 'Creating validators';
+$_lang['mc_creating_class_files'] = 'Creating class files';
+$_lang['mc_mycomponent_not_installed_export'] = 'MyComponent must be installed to export the Project from MODx!';
+$_lang['mc_export_objects'] = 'Export Objects';
+$_lang['mc_updating_project_config'] = 'Updating Project Config file';
+$_lang['mc_import_objects'] = 'Import Objects';
+$_lang['mc_skipping_static_element'] = 'Skipping static element';
+$_lang['mc_would_be_updating'] = 'Would be updating';
+$_lang['mc_setting_properties_for'] = 'Setting properties for';
+$_lang['mc_updated'] = 'Updated';
+$_lang['mc_element_nf'] = 'Could not find element';
+$_lang['mc_remove_objects'] = 'Remove Objects';
+$_lang['mc_removing_project_files'] = 'Removing project files';
+$_lang['mc_files_and_directories_removed'] = 'Project files and directories removed';
+$_lang['mc_vc_files_may_remain'] = 'Project files removed, but Version Control files may remain';
+$_lang['mc_initial_memory_used'] = 'Initial Memory Used';
+$_lang['mc_megabytes'] = 'megabytes';
+$_lang['mc_final_memory_used'] = 'Final Memory Used';
+$_lang['mc_peak_memory_used'] = 'Peak Memory Used';
+$_lang['mc_total_time'] = 'Total time';
+
+/* used in LexiconHelper or its included classes  */
+
+$_lang['mc_lexicon_helper'] = 'Lexicon Helper';
+$_lang['mc_found_these_class_files'] = 'Found these class files';
+$_lang['mc_stars'] = '*********************************************';
+$_lang['mc_processing_element'] = 'Processing Element';
+$_lang['mc_removing'] = 'Removing';
+$_lang['mc_strings_from_code_files'] = 'strings from code files';
+$_lang['mc_rewriting_code_file'] = 'rewriting code file';
+$_lang['mc_no_code_file_at'] = 'No Code file at';
+$_lang['mc_no_language_strings_in_file'] = 'No language strings in file';
+$_lang['mc_lexicon_file_nf'] = 'Cannot find lexicon file';
+$_lang['mc_no_missing_strings_in_lex_file'] = 'No missing strings in lexicon file!';
+$_lang['mc_could_not_open_lex_file'] = 'Could not open lexicon file';
+$_lang['mc_updated_lex_file'] = 'Updated Lexicon file';
+$_lang['mc_with_these_strings'] = 'with these strings';
+$_lang['mc_cannot_update_multiple_lex_files'] = 'Cannot update multiple Lexicon files';
+$_lang['mc_paste_these_strings'] = 'paste these strings in the appropriate file';
+$_lang['mc_missing_lex_strings'] = 'Missing Lexicon strings';
+$_lang['mc_lex_strings_never_used'] = 'The following lexicon strings never used in code';
+$_lang['mc_no_unused_lex_strings'] = 'No unused strings in lexicon files!';
+$_lang['mc_all_lex_strings_defined'] = 'All lexicon strings are defined in lexicon files!';
+$_lang['mc_no_empty_lex_strings_in_files'] = 'No Empty Lexicon strings in lexicon files!';
+$_lang['mc_empty_lex_strings'] = 'The following lexicon strings are in a lexicon file, but have no value:';
+$_lang['mc_final_audit'] = 'Final Audit';
+$_lang['mc_checking_for_property_lex_strings'] = 'Checking for property description lexicon strings';
+$_lang['mc_checking_properties_for'] = 'Checking Properties for';
+$_lang['mc_no_properties_in'] = 'No properties in';
+$_lang['mc_no_properties_file_for'] = 'No properties file for';
+$_lang['mc_no_missing_property_descriptions_in_lex_file'] = 'No missing property descriptions in lexicon file!';
+$_lang['mc_no_empty_property_descriptions_in_lex_file'] = 'No empty property descriptions in lexicon file!';
+$_lang['mc_no_missing_property_description_lex_strings'] = 'No missing property description lexicon strings!';
+$_lang['mc_entries_with_these_keys'] = 'entries with these keys';
+$_lang['mc_could_not_open_lex_properties_file'] = 'Could not open lexicon properties file for writing';
+$_lang['mc_failed_to_update_lex_file'] = 'Failed to update lexicon file; check for syntax errors in the lexicon file or lexicon strings';
+$_lang['mc_lex_properties_code_to_add'] = 'Code to add to lexicon properties file:';
+$_lang['mc_checking_for_ss_lex_strings'] = 'Checking for System Setting lexicon strings';
+$_lang['mc_checking_ss_names_descriptions'] = 'Checking System Setting names and descriptions';
+$_lang['mc_no_lex_strings_in_dip'] = 'No lexicon strings in default.inc.php';
+$_lang['mc_could_not_open_dip_append'] = 'Could not open default.inc.php for appending';
+$_lang['mc_could_not_open_dip_write'] = 'Could not open default.inc.php for writing';
+$_lang['mc_update_these_strings_in dip'] = 'Updated these strings in default.inc.php';
+$_lang['mc_strings_missing_from_dip'] = 'These strings are missing from default.inc.php';
+$_lang['mc_no_ss_names'] = 'No System Setting names to check';
+$_lang['mc_lex_element_is_empty'] = 'Element is empty';
+$_lang['mc_file_is_empty'] = 'File is empty';
+
+/* used in CheckProperties or its included classes  */
+$_lang['mc_check_properties'] = 'Check Properties';
+
+/* used in build.transport.php */
+$_lang['mc_error'] = 'ERROR';
+$_lang['mc_no_config_file'] = 'Could not get project config file';
+$_lang['mc_critical_setting_not_set'] = 'Critical setting is not set in Project Config';
+$_lang['mc_space_hyphen_warning'] = 'packageNameLower cannot contain spaces or hyphens';
+$_lang['no_categories'] = 'No Categories';
+$_lang['mc_build'] = 'Build';
+$_lang['mc_created_package'] = 'Created Package';
+$_lang['mc_created_namespace'] = 'Created namespace';
+$_lang['mc_contexts_not_an_array'] = 'contexts not an array.';
+$_lang['mc_packaged'] = 'Packaged';
+$_lang['mc_resources_not_an_array'] = 'Resources not an array';
+$_lang['mc_resources'] = 'resources';
+$_lang['mc_settings_not_an_array'] = 'Settings not an array';
+$_lang['mc_new_system_settings'] = 'new System Settings';
+$_lang['mc_context_settings_not_an_array'] = 'Context Settings not an array';
+$_lang['mc_context_settings'] = 'Context Settings';
+$_lang['mc_could_not_open'] = 'Could not open outfile';
+$_lang['mc_could_not_open_js_directory'] = 'Could not open JS directory';
+$_lang['mc_creating_category'] = 'Creating Category';
+$_lang['mc_processing_elements_in_category'] = 'Processing Elements in Category';
+$_lang['mc_snippets'] = 'Snippets';
+$_lang['mc_adding_snippets_failed'] = 'Adding Snippets failed';
+$_lang['mc_non_array_in'] = 'Non-array in';
+$_lang['mc_property_sets'] = 'Property Sets';
+$_lang['mc_chunks'] = 'Chunks';
+$_lang['mc_adding_chunks_failed'] = 'Adding Chunks failed';
+$_lang['mc_adding_templates'] = 'Adding Templates';
+$_lang['mc_templates'] = 'Templates';
+$_lang['mc_adding_templates_failed'] = 'Adding Templates failed';
+$_lang['mc_adding_template_variables'] = 'Adding Template Variables';
+$_lang['mc_tvs'] = 'TVs';
+$_lang['mc_adding_tvs_failed'] = 'Adding TVs failed';
+$_lang['mc_plugins'] = 'Plugins';
+$_lang['mc_adding_plugins_failed'] = 'Adding Plugins failed';
+$_lang['mc_processing_validators'] = 'Processing Validators';
+$_lang['mc_packaging'] = 'Packaging';
+$_lang['mc_processing_resolvers'] = 'Processing Resolvers';
+$_lang['mc_packaged_core_files'] = 'Packaged core directory files';
+$_lang['mc_packaged_assets_files'] = 'Packaged assets directory files';
+$_lang['mc_packaging_subpackages'] = 'Packaging subpackages';
+$_lang['mc_packaging_menu'] = 'Packaging menu';
+$_lang['mc_could_not_package_menu'] = 'Could not package menu';
+$_lang['mc_menu_items'] = 'menu items';
+$_lang['mc_package_built'] = 'Package Built';
+$_lang['mc_execution_time'] = 'Execution time';
