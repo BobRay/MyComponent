add62b1b805d1c5bea941981e1d07c1e37392921
bobray <bobray@softville.com>
2012-12-08 20:03:59 -0600

    default resolver to delete mycomponent config file

---
+++ _build/resolvers/mycomponent.resolver.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Resolver for MyComponent extra
+ *
+ * Copyright 2012 by Bob Ray <http://bobsguides.com>
+ * Created on 12-08-2012
+ *
+ * MyComponent is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MyComponent is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MyComponent; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * @package mycomponent
+ * @subpackage build
+ */
+
+/* @var $object xPDOObject */
+/* @var $modx modX */
+
+/* @var array $options */
+
+if ($object->xpdo) {
+    $modx =& $object->xpdo;
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            $path = MODX_CORE_PATH . 'components/mycomponent/_build/config/mycomponent.config.php';
+            unlink($path);
+            break;
+
+        case xPDOTransport::ACTION_UNINSTALL:
+            break;
+    }
+}
+
+return true;
