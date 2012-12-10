38dc50f66c775137ee52dfe16590297671f985c9
bobray <bobray@softville.com>
2012-12-09 20:34:19 -0600

    version to 3.0.4

--- _build/config/mycomponent.config.php
+++ _build/config/mycomponent.config.php
@@ -8,7 +8,7 @@ $components = array(
     /* No spaces, no dashes */
     'packageNameLower' => $packageNameLower,
     'packageDescription' => 'MyComponent MODX Extra development tool',
-    'version' => '3.0.3',
+    'version' => '3.0.4',
     'release' => 'beta1',
     'author' => 'Bob Ray',
     'email' => '<http://bobsguides.com>',
