38ad15b7f1c983c429d64be4634713b6d8672143
bobray <bobray@softville.com>
2013-11-02 23:19:53 -0500

    Version to 3.1.2-pl -- released

--- _build/config/mycomponent.config.php
+++ _build/config/mycomponent.config.php
@@ -89,7 +89,7 @@ $components = array(
     /* ************************ NAMESPACE(S) ************************* */
     /* (optional) Typically, there's only one namespace which is set
      * to the $packageNameLower value. Paths should end in a slash
-    */
+    */ 
 
     'namespaces' => array(
         'example' => array(
