5a8e3cc6ecf0aaf0bd4b2b1b5a092c27eae6dc45
bobray <bobray@softville.com>
2018-08-08 10:41:19 -0500

    Handle new menu objects without related action object

--- core/components/mycomponent/model/mycomponent/menuadapter.class.php
+++ core/components/mycomponent/model/mycomponent/menuadapter.class.php
@@ -16,18 +16,18 @@ class MenuAdapter extends ObjectAdapter {
                 $fields['namespace'] = $this->helpers->getProp('packageNameLower');
             }
             $this->myFields = $fields;
-            $actionFields = $modx->getOption('action', $fields, array());
+            /*$actionFields = $modx->getOption('action', $fields, array());
 
             if (empty($actionFields)) {
                 $helpers->sendLog(modX::LOG_LEVEL_ERROR, '    ' .
                     $this->modx->lexicon('mc_menu_has_no_action'));
                 return;
-            }
-            if (empty($actionFields['namespace'])) {
+            }*/
+           /* if (empty($actionFields['namespace'])) {
                 $helpers->sendLog(modX::LOG_LEVEL_ERROR, '    ' .
                     $this->modx->lexicon('mc_menu_action_no_namespace'));
                 return;
-            }
+            }*/
             ObjectAdapter::$myObjects['menus'][] = $fields;
             $this->myFields = $fields;
 
@@ -42,13 +42,13 @@ class MenuAdapter extends ObjectAdapter {
         $fields = $this->myFields;
         $obj = $this->modx->getObject('modMenu', array('text' => $fields['text'], 'parent' => $fields['parent']));
         if (! $obj) {
-            $actionFields = $fields['action'];
+           /* $actionFields = $fields['action'];
             $action = $this->modx->newObject('modAction');
             $action->fromArray($actionFields, '', true, true);
-            unset($fields['action'], $fields['id']);
+            unset($fields['action'], $fields['id']);*/
             $menu = $this->modx->newObject('modMenu');
             $menu->fromArray($fields, '', true, true);
-            $menu->addOne($action);
+            // $menu->addOne($action);
             if ($menu->save()) {
                 $this->helpers->sendLog(modX::LOG_LEVEL_INFO, '    ' .
                     $this->modx->lexicon('mc_created_menu')
@@ -75,8 +75,8 @@ class MenuAdapter extends ObjectAdapter {
         if ($mode == MODE_BOOTSTRAP) {
             $menus = $helpers->modx->getOption('menus', ObjectAdapter::$myObjects, array());
             foreach($menus as $menu => $fields) {
-                $actionFields[] = $fields['action'];
-                unset($fields['action']);
+                /*$actionFields[] = $fields['action'];
+                unset($fields['action']);*/
                 $menuFields[] = $fields;
             }
         } elseif ($mode == MODE_EXPORT) {
@@ -85,25 +85,16 @@ class MenuAdapter extends ObjectAdapter {
             foreach($nameSpaces as $namespace => $fields) {
                 $name = isset($fields['name']) ? $fields['name'] : $namespace;
                 $name = strtolower($name);
-                $actions = $helpers->modx->getCollection('modAction', array('namespace' => $name));
-                foreach($actions as $action) {
-                    /* @var $action modAction */
+                $menus = $helpers->modx->getCollection('modMenu', array('namespace' => $name));
+                foreach($menus as $menu) {
                     /* @var $menu modMenu */
-                    $menus = $action->getMany('Menus');
-                    foreach($menus as $menu) {
                         $m_fields = $menu->toArray();
                         if (!isset($m_fields['namespace'])) {
                             $m_fields['namespace'] = !empty($namespace) ? $namespace
                                 : $helpers->getProp('packageNameLower');
                         }
-                        unset($m_fields['action']);
-                        $menuFields[] = $m_fields;
-                        $actionObj = $menu->getOne('Action');
-                        $a_fields = $actionObj->toArray();
-                        unset($a_fields['id']);
-                        $actionFields[] = $a_fields;
-                    }
 
+                        $menuFields[] = $m_fields;
                 }
 
             }
@@ -119,12 +110,12 @@ class MenuAdapter extends ObjectAdapter {
             $i = 0;
             foreach($menuFields as $k => $fields) {
                 $code = '';
-                $actionFields[$i]['id'] = $i + 1;
-                /* do Action */
+               /* $actionFields[$i]['id'] = $i + 1;
+                  // do Action
                 $code .= "\$action = \$modx->newObject('modAction');\n";
                 $code .= "\$action->fromArray( ";
                 $code .= var_export($actionFields[$i], true);
-                $code  .= ", '', true, true);\n";
+                $code  .= ", '', true, true);\n";*/
 
                 /* do Menu item */
                 $menuFields[$i]['id'] = $i + 1;
@@ -137,10 +128,10 @@ class MenuAdapter extends ObjectAdapter {
                 $code .= $i + 1 . ']->fromArray( ';
                 $code .= var_export($menuFields[$i], true);
                 $code .= ", '', true, true);\n";
-                $code .= "\$";
+               /* $code .= "\$";
                 $code .= "menus[";
                 $code .= $i + 1 . ']->addOne(';
-                $code .= "\$action);\n";
+                $code .= "\$action);\n";*/
 
 
                 $tpl .= $code;
@@ -166,7 +157,7 @@ class MenuAdapter extends ObjectAdapter {
         /* @var $menu modMenu */
         $menu = $this->modx->getObject('modMenu', array('text' => $fields['text'], 'parent' => $fields['parent']));
         if ($menu) {
-            $action = $menu->getOne('Action');
+           /* $action = $menu->getOne('Action');
             if ($action) {
                 if ($action->remove()) {
                     $temp = $this->modx->setLogLevel(modX::LOG_LEVEL_INFO);
@@ -175,11 +166,11 @@ class MenuAdapter extends ObjectAdapter {
                         . ': ' . $fields['text']);
                     $this->modx->setLogLevel($temp);
                 }
-            }
+            }*/
             if ($menu->remove()) {
                 $temp = $this->modx->setLogLevel(modX::LOG_LEVEL_INFO);
                 $this->helpers->sendLog(modX::LOG_LEVEL_INFO, '    ' .
-                    $this->modx->lexicon('mc_removed_action')
+                    $this->modx->lexicon('mc_removed_menu item')
                     . ': '. $fields['text']);
                 $this->modx->setLogLevel($temp);
             }
