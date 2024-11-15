2d90398a0e24a342d471e405e1c91eec7a85442e
bobray <bobray@softville.com>
2024-11-15 16:29:13 -0600

    Add commented out autoloader in _bootstrap.php

--- _build/tests/_bootstrap.php
+++ _build/tests/_bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 chdir('C:\xampp\htdocs\addons\assets\mycomponents\mycomponent\_build');
+
+/* C:\xampp\htdocs\addons\assets\mycomponents\mycomponent\core\components\mycomponent\model\mycomponent\mcautoload.php */
+/*require dirname(__FILE__, 3) . '/core/components/mycomponent/model/mycomponent/mcautoload.php';
+spl_autoload_register('mc_auto_load');*/
+
 $x = 1;
