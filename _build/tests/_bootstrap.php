ecfda167e29d83d7643e814a5775985720ffefe6
bobray <bobray@softville.com>
2024-12-02 16:52:06 -0600

    Require mc autoload and assert functions in unit _bootstrap

--- _build/tests/_bootstrap.php
+++ _build/tests/_bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 chdir('C:\xampp\htdocs\addons\assets\mycomponents\mycomponent\_build');
 
+require_once 'c:/xampp/htdocs/addons/vendor/autoload.php';
+require_once 'c:/xampp/htdocs/addons/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+
+
 /* C:\xampp\htdocs\addons\assets\mycomponents\mycomponent\core\components\mycomponent\model\mycomponent\mcautoload.php */
 /*require dirname(__FILE__, 3) . '/core/components/mycomponent/model/mycomponent/mcautoload.php';
 spl_autoload_register('mc_auto_load');*/
