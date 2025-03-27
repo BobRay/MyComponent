53ab3368b2037761ee561afd7e21530d0cc74e95
bobray <bobray@softville.com>
2025-03-27 14:30:29 -0500

    MODX 2 and 3 dynamic processor class in cmp.processor.class.php

--- core/components/mycomponent/elements/chunks/cmp.processor.class.php
+++ core/components/mycomponent/elements/chunks/cmp.processor.class.php
@@ -12,9 +12,36 @@
  */
 
 /* @var $modx modX */
+/* @var $modx modX */
+if (!defined('MODX_CORE_PATH')) {
+    include dirname(__FILE__, 5) . '/config.core.php';
+    if (empty(MODX_CORE_PATH)) {
+        /* For dev. environment */
+        include dirname(__FILE__, 8) . '/config.core.php';
+    }
 
+}
 
-class mc_ProcessorTypeProcessor extends modProcessor {
+if (empty(MODX_CORE_PATH)) {
+    die('Could not find config.core.php');
+}
+
+$v = include MODX_CORE_PATH . 'docs/version.inc.php';
+$isMODX3 = $v['version'] >= 3;
+
+if ($isMODX3) {
+    abstract class DynamicBaseProcessorParent extends MODX\Revolution\Processors\Processor {
+    }
+} else {
+    if (!class_exists('modProcessor')) {
+        include MODX_CORE_PATH . 'model\modx\modprocessor.class.php';
+    }
+
+    abstract class DynamicBaseProcessorParent extends modProcessor {
+    }
+}
+
+class mc_ProcessorTypeProcessor extends DynamicBaseProcessorParent {
     public $classKey = 'modmc_Element';
     public $languageTopics = array('mc_packageNameLower:default');
     public $defaultSortField = 'name';
